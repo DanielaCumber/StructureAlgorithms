99b46416ce577cb1935ebeaed794c63cf57a114e
DanielaCumber <138382430+DanielaCumber@users.noreply.github.com>
2024-07-23 16:51:35 +0200

    task3 resolved (with annotations)

--- Arrays/Task3.py
+++ Arrays/Task3.py
@@ -1,28 +1,27 @@
 # How do you find the largest and smallest number in an unsorted integer array?
 # Data generator
+
 import numpy as np
 
-np.random.seed(0)
+np.random.seed(1)
 original_array = np.arange(1, 101)
 np.random.shuffle(original_array)
 
-Function find_min_max(numbers)
-    Set min_num and max_num on the first number of numbers
-    For each number in numbers from the second number onwards
-        If the number is less than num_min
-            Set num_min to the number
-        If the number is greater than max_num
-            Set max_num to the number
-    Returns min_num and max_num
-End of function
+def find_min_and_max(numbers):  # Define la función que toma una lista de números
 
-Generate an array of numbers from 1 to 100 and shuffle it to get original_array
+    # minimum_number = min(numbers)
+    # max_num = max(numbers)
+    # return minimum_number, max_num
 
-Call find_min_max with original_array and store results in min_num and max_num
+    min_number = max_number = numbers[0]  # is a form of multiple assignment in Python. the assignment is made from right to left
+    for num in numbers[1:]:  # iterates over all the elements of the Numbers list, starting from the second element (index 1) to the end. The notation numbers[1:] means "all elements of numbers from index 1 to the end".
+        if num < min_number:  # Inside the loop, this line checks if the current element "num" is less than the value stored in min_val.
+            min_number = num  # If "num" is smaller than min_val, then min_val is updated to be "num". This ensures that min_val always contains the smallest value found so far in the list.
+        if num > max_number:  # If "num" is not less than min_val, this line checks if "num" is greater than the value stored in max_val.
+            max_number = num  # If "num" is greater than max_val, then max_val is updated to be "num". This ensures that max_val always contains the largest value found so far in the list.
 
-Print min_num and max_num
+    return min_number, max_number
 
-
-def numbers
-
+min_val, max_val = find_min_and_max(original_array)  # These are two variables used to store the values returned by the min_and_max function. This is the call to the min_and_max function with the original array_array as argument
+print(f"Valor mínimo: {min_val}, Valor máximo: {max_val}")
 
